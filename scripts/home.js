134c0c61ff5d66bd91a52fddcb58b0d5f40b6a79
watwatos <alichamas.22@hotmail.com>
2024-03-03 14:18:13 +0200

    finished-info-section

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,5 @@
+
+//hero section images
 let images =['slide1.jpg','slide2.jpg','slide3.jpg','slide4.jpg','slide5.jpg','slide6.jpg','slide7.jpg']
 const slider = document.getElementById('slider')
 
@@ -6,6 +8,8 @@ setInterval(function(){
     slider.src=`./assets/hero-section/${images[index]}`
 },4000)
 
+
+//scroling nav
 const header=document.getElementById('header')
 const logo = document.getElementById('logo')
 
@@ -31,3 +35,59 @@ window.addEventListener('scroll',function(){
         
     }
 })
+
+//info cards
+let infoCards=[
+    {
+        image:'1.png',
+        title:'Community',
+        desc:'An environment catering to creative minds, providing the framework for ambitious and sustainable communities. BDD creates a community for collaboration, facilitates the sharing of ideas and connects decision makers with entrepreneurs.',
+        color:'rgb(43, 43, 187,0.1)'
+    },
+    {
+        image:'2.png',
+        title:'Sharing Economy',
+        desc:'A business district providing value added services and cutting edge facilities for developing, producing and exchanging innovative tools and applications.',
+        color:'rgb(43, 43, 187,0.1)'
+    },
+    {
+        image:'3.png',
+        title:'Smart Design',
+        desc:'A new urban topography that reflects the creative community with innovative and ground breaking architecture seamlessly integrating the latest technological solutions.',
+        color:'rgba(73, 238, 95, 0.169)'
+    },
+    {
+        image:'4.png',
+        title:'Sustainability',
+        desc:'An urban environment focused on minimizing primary energy consumption and reducing the impact of the built environment on human health and nature, with the aim of achieving LEED silver rating.',
+        color:'rgba(238, 73, 188, 0.279)',
+    },
+    {
+        image:'5.png',
+        title:'Social Well Being',
+        desc:'A high tech and high quality environment that allows new ways of working and promotes change for more flexible work practices and lifestyles.',
+        color:'rgba(227, 238, 73, 0.279)'
+    }
+]
+
+const infoCardsContainer=document.getElementById('info-card-container');
+
+for(let i=0;i<infoCards.length;i++){
+    infoCardsContainer.innerHTML+=` <div class="info_card flex primary-bg card-p gap column center">
+                                    <img src="./assets/info-section/${infoCards[i].image}" alt="">
+                                    <h2>${infoCards[i].title}</h2>
+                                    <p>${infoCards[i].desc}</p>
+                                </div>  `
+
+}
+
+let infoCard= document.querySelectorAll('.info_card');
+
+for(let i=0;i<infoCard.length;i++){
+    infoCard[i].addEventListener('mouseover',function(){
+        infoCard[i].style.backgroundColor=infoCards[i].color
+    })
+    infoCard[i].addEventListener('mouseleave',function(){
+        infoCard[i].style.backgroundColor='white'
+    })
+}
